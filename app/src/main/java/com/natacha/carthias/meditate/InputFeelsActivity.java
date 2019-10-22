87aeae5c9de60e902e507148fa08e2cb70ac41cd
Natacha <natacha.m.carthias@gmail.com>
2019-10-22 15:44:47 +0200

    InputFeelsActivity slide transition added

--- app/src/main/java/com/natacha/carthias/meditate/InputFeelsActivity.java
+++ app/src/main/java/com/natacha/carthias/meditate/InputFeelsActivity.java
@@ -4,12 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class InputFeelsActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,7 +21,12 @@ public class InputFeelsActivity extends AppCompatActivity implements View.OnClic
     ImageView imgViewCalm;
     ImageView imgViewBoost;
     ImageView imgViewRest;
-
+    Scene scene1;
+    Scene scene2;
+    Scene currentScene;
+    ViewGroup sceneRoot;
+    Transition transition;
+    int click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,25 @@ public class InputFeelsActivity extends AppCompatActivity implements View.OnClic
 
         ThemeUtils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_input_feels);
+
+
+        imgViewCalm = findViewById(R.id.img_calm);
+        imgViewBoost = findViewById(R.id.img_boost);
+        imgViewRest = findViewById(R.id.img_rest);
+
+        sceneRoot = (ViewGroup) findViewById(R.id.sceneRoot);
+
+
+        // InputFeels Transition Animation
+        scene1 = Scene.getSceneForLayout(sceneRoot, R.layout.scene1, this);
+        scene2 = Scene.getSceneForLayout(sceneRoot, R.layout.scene2,this);
+
+        transition = TransitionInflater.from(this).inflateTransition(R.transition.slide_in);
+
+        scene1.enter();
+        currentScene = scene1;
+
+
 
 
     }
@@ -59,6 +87,9 @@ public class InputFeelsActivity extends AppCompatActivity implements View.OnClic
     // Start Meditate Activity passing the relevant Meditation and Image
     @Override
     public void onClick(View view) {
+
+        // InputFeels Slide Transition Animation on first click
+        // Start Meditate Activity on second click
         switch (view.getId()) {
             case R.id.img_calm:
                 Intent intent = new Intent(InputFeelsActivity.this, MeditateActivity.class);
@@ -66,25 +97,60 @@ public class InputFeelsActivity extends AppCompatActivity implements View.OnClic
                 bundle.putString("Meditation", "Calming");
                 bundle.putInt("Image",R.drawable.img_calming);
                 intent.putExtras(bundle);
+                if (currentScene == scene1) {
+                    TransitionManager.go(scene2, transition);
+                    currentScene = scene2;
+                    click=click+1;}
+                else if (currentScene == scene2) {
+                    TransitionManager.go(scene1, transition);
+                    currentScene = scene1;
+                    click=click+1;
+                }
+                if (click == 2) {
                 startActivity(intent);
+                    click = 0;}
               break;
             case R.id.img_boost:
-                Intent intent1 = new Intent(InputFeelsActivity.this, MeditateActivity.class);
-                Bundle bundle1 = new Bundle();
-                bundle1.putString("Meditation", "Boosting");
-                bundle1.putInt("Image",R.drawable.img_boosting);
-                intent1.putExtras(bundle1);
-                startActivity(intent1);
+                intent = new Intent(InputFeelsActivity.this, MeditateActivity.class);
+                bundle = new Bundle();
+                bundle.putString("Meditation", "Boosting");
+                bundle.putInt("Image",R.drawable.img_boosting);
+                intent.putExtras(bundle);
+                if (currentScene == scene1) {
+                    TransitionManager.go(scene2, transition);
+                    currentScene = scene2;
+                    click=click+1;}
+                else if (currentScene == scene2) {
+                    TransitionManager.go(scene1, transition);
+                    currentScene = scene1;
+                    click=click+1;
+                }
+                if (click == 2) {
+                    startActivity(intent);
+                    click = 0;}
                 break;
             case R.id.img_rest:
-                Intent intent2 = new Intent(InputFeelsActivity.this, MeditateActivity.class);
-                Bundle bundle2 = new Bundle();
-                bundle2.putString("Meditation", "Restful");
-                bundle2.putInt("Image",R.drawable.img_restful);
-                intent2.putExtras(bundle2);
-                startActivity(intent2);
+                intent = new Intent(InputFeelsActivity.this, MeditateActivity.class);
+                bundle = new Bundle();
+                bundle.putString("Meditation", "Restful");
+                bundle.putInt("Image",R.drawable.img_restful);
+                intent.putExtras(bundle);
+                if (currentScene == scene1) {
+                    TransitionManager.go(scene2, transition);
+                    currentScene = scene2;
+                    click=click+1;}
+                else if (currentScene == scene2) {
+                    TransitionManager.go(scene1, transition);
+                    currentScene = scene1;
+                    click=click+1;
+                }
+                if (click == 2) {
+                    startActivity(intent);
+                click = 0;}
                 break;
         }
 
+        }
+
     }
-}
+
